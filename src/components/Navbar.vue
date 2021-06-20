772808cf89fc0b12e2caf24e1888fddb78b28ab8
Wade <wsw0615@gmail.com>
2021-06-20 17:53:39 +0800

    feat: set router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+        width="100"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <router-link :to="{name: 'About'}">
+      <span class="mr-2 white--text">To about</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </router-link>
+  </v-app-bar>
+</template>
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import HelloWorld from './HelloWorld.vue';
+import Navbar from './Navbar.vue';
+
+Vue.component('HelloWorld', HelloWorld);
+Vue.component('Navbar', Navbar);
