9bad595499c6171472e6d9c9cb856ddf8722bfba
Wade <wsw0615@gmail.com>
2021-07-28 13:26:38 +0800

    feat: schedule-setting selected

--- packages/client/src/views/ScheduleSetting.vue
+++ packages/client/src/views/ScheduleSetting.vue
@@ -1,14 +1,34 @@
 <template>
-  <div id="schedule-setting" class="pa-4 pl-16">
-    schedule-setting
+  <div id="schedule-setting" class="">
+    <v-select
+      :items="items"
+      item-text="name"
+      item-value="name"
+      label="Solo field"
+      solo
+    >
+      <!-- <template v-slot:item="{ item }">
+        <v-list dense class="pa-0">
+          <v-list-item>
+            <v-icon small v-if="item.jobTitle === 'FT'">mdi-pencil</v-icon>
+            <v-icon small v-if="item.jobTitle === 'PT'">mdi-check</v-icon>
+            {{ item.name }}
+          </v-list-item>
+        </v-list>
+      </template> -->
+    </v-select>
   </div>
 </template>
 
 <script>
+import mockStaffs from '@/mock/staffs.json';
+
 export default {
   name: 'ScheduleSetting',
   data: () => ({
+    items: mockStaffs,
   }),
+  methods: {},
 };
 </script>
 
