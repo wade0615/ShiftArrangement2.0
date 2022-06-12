ab18eb174faf10591a584cd417e9e47cc4c2c745
Wade <>
2022-06-12 20:56:16 +0800

    feat: set nav route

--- packages/client/components/NavBar.vue
+++ packages/client/components/NavBar.vue
@@ -7,13 +7,15 @@
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
         <v-subheader>分頁清單</v-subheader>
-        <nuxt-link to="/" class="col-12 col-md-6">註冊頁</nuxt-link>
         <v-list-item-group
           v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
           <v-list-item v-for="(item, index) in list" :key="index + 'list'">
-            <v-list-item-title>{{ item }}</v-list-item-title>
+            <nuxt-link :to="item.route" class="col-12 col-md-6">{{
+              item.label
+            }}</nuxt-link>
+            <v-list-item-title></v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -27,14 +29,38 @@ export default {
     drawer: false,
     group: null,
     list: [
-      '控制台',
-      '通知',
-      '排班管理',
-      '統計報表',
-      '員工管理',
-      '店面管理',
-      '設定',
-      '註冊/登入',
+      {
+        route: '/',
+        label: '控制台',
+      },
+      {
+        route: '/',
+        label: '通知',
+      },
+      {
+        route: '/',
+        label: '排班管理',
+      },
+      {
+        route: '/',
+        label: '統計報表',
+      },
+      {
+        route: '/',
+        label: '員工管理',
+      },
+      {
+        route: '/',
+        label: '店面管理',
+      },
+      {
+        route: '/',
+        label: '設定',
+      },
+      {
+        route: '/register',
+        label: '註冊/登入',
+      },
     ],
   }),
   watch: {
--- packages/client/nuxt.config.js
+++ packages/client/nuxt.config.js
@@ -42,8 +42,19 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+    https: true,
+    proxy: true,
+    prefix: '/api/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  proxy: {
+    '/api/': {
+      target: process.env.API_BASE_URL,
+      secure: false,
+      pathRewrite: { '^/api/': '' },
+    },
+  },
 }
